657fe789b0e0977f5ade2e25c4135e4e64baeca4
focus110 <iamemmaochuko@gmail.com>
2022-10-10 12:38:53 +0100

    made hourlyforecast component responsive

--- src/component/HourlyForecast.jsx
+++ src/component/HourlyForecast.jsx
@@ -23,10 +23,14 @@ const HourlyForecast = () => {
   const hourlyForcast = hrWeather.map((item, i) => {
     return (
       <div className="items-center space-y-2 bg-transparent rounded-2xl p-4">
-        <img className="object-contain w-24 h-24" src={cloudy} alt="" />
+        <img
+          className="object-contain w-16 h-16 sm:w-24 sm:h-24"
+          src={cloudy}
+          alt=""
+        />
 
-        <h2 className="font-bold text-white text-3xl">18°C</h2>
-        <h2 className="font-medium text-white text-2xl">Cloudy</h2>
+        <h2 className="font-bold text-white sm:text-3xl">18°C</h2>
+        <h2 className="font-medium text-white sm:text-2xl">Cloudy</h2>
       </div>
     );
   });
@@ -36,8 +40,8 @@ const HourlyForecast = () => {
       <h2 className="text-left font-medium text-2xl text-white">
         Hourly Forcast
       </h2>
-      <div className="bg-white bg-opacity-10 rounded-lg px-8 overflow-x-scroll hide__scrollbar">
-        <div className="flex items-center text-center space-x-8 py-4">
+      <div className="bg-white bg-opacity-10 rounded-lg px-2 sm:px-8 overflow-x-scroll hide__scrollbar">
+        <div className="flex items-center text-center sm:space-x-8 sm:py-4">
           {hourlyForcast}
         </div>
       </div>
