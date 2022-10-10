bd24a3d030c46ead84945a565df1cb0611c4f78b
focus110 <iamemmaochuko@gmail.com>
2022-10-10 12:33:10 +0100

    hourly forecast component

--- src/component/HourlyForecast.jsx
+++ src/component/HourlyForecast.jsx
@@ -13,12 +13,17 @@ const HourlyForecast = () => {
     {},
     {},
     {},
+    {},
+    {},
+    {},
+    {},
+    {},
   ]);
 
   const hourlyForcast = hrWeather.map((item, i) => {
     return (
       <div className="items-center space-y-2 bg-transparent rounded-2xl p-4">
-        <img className="w-24 h-24" src={cloudy} alt="" />
+        <img className="object-contain w-24 h-24" src={cloudy} alt="" />
 
         <h2 className="font-bold text-white text-3xl">18°C</h2>
         <h2 className="font-medium text-white text-2xl">Cloudy</h2>
@@ -31,7 +36,7 @@ const HourlyForecast = () => {
       <h2 className="text-left font-medium text-2xl text-white">
         Hourly Forcast
       </h2>
-      <div className="bg-white bg-opacity-10 rounded-lg px-8 overflow-x-scroll">
+      <div className="bg-white bg-opacity-10 rounded-lg px-8 overflow-x-scroll hide__scrollbar">
         <div className="flex items-center text-center space-x-8 py-4">
           {hourlyForcast}
         </div>
