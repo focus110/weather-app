6569c52d52bb2dc5b3681a291507dc27c31872e0
focus110 <iamemmaochuko@gmail.com>
2022-10-10 17:11:09 +0100

    made weatherDetails component responsive

--- src/component/WeatherDetails.jsx
+++ src/component/WeatherDetails.jsx
@@ -4,11 +4,14 @@ import Title from "./Title";
 
 const WeatherDetails = () => {
   return true ? (
-    <div className="text-center py-8 sm:px-96">
-      <div className="flex flex-col justify-center space-y-8 bg-white bg-opacity-10 rounded-lg p-4 sm:p-8">
+    <div className="text-center pb-8 sm:px-96">
+      <div className="flex flex-col justify-center space-y-8 bg-white bg-opacity-10 rounded-lg p-8">
         <Title title="Current Weather" />
-        <div className="flex justify-between items-center space-x-4">
+        <div className="flex flex-col sm:flex-row justify-between items-center space-x-4">
           <img className="object-contain w-16 h-16 " src={cloudy} alt="" />
+          <h2 className="sm:hidden sm:text-left font-semibold text-white text-3xl">
+            18°C
+          </h2>
           <div>
             <h2 className="flex space-x-2 font-medium text-white text-xl">
               {/* <svg
@@ -29,14 +32,14 @@ const WeatherDetails = () => {
                   d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
                 />
               </svg> */}
-              <span>Boston, MA, United State</span>
+              <span className=" text-sm">Boston, MA, United State</span>
             </h2>
-            <p className="text-gray-400 font-light text-left">
+            <p className="text-gray-400 font-light sm:text-left">
               12:52PM | <span>Cloudy</span>
             </p>
           </div>
           <div>
-            <h2 className="sm:text-left font-semibold text-white text-3xl">
+            <h2 className="hidden sm: block sm:text-left font-semibold text-white text-3xl">
               18°C
             </h2>
             {/* <h2 className="sm:text-left ml-1 font-medium text-white text-2xl">
